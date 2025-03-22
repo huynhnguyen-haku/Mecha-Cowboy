995b80d457dc36edb314745518c4dfed9c0a414c
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-03-22 09:49:42 +0700

    Add rig aim constrants and animation layers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -68,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
             aimDirection.Normalize();
 
             transform.forward = aimDirection;
-            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y + 1, hitInfo.point.z);
         }
     }
 
