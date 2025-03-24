5861aaa904b17319cf31cc1a1589cf9abc3f99db
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-03-24 14:07:01 +0700

    Cleanup and add effect for bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
+
     private Rigidbody rb;
 
     private void Start()
@@ -11,8 +13,19 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(collision);
         Destroy(gameObject); // Destroy the bullet on collision
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contact = collision.contacts[0];
+            GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+
+            Destroy(newImpactFX, 1);
+        }
     }
 }
 
