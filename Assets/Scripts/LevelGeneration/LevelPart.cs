a46aaccc354f31fd41ddec1d6d8c4f1a24b7f3b2
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-04-16 10:35:32 +0700

    Setup level generator

---
+++ Assets/Scripts/LevelGeneration/LevelPart.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPart : MonoBehaviour
+{
+    public void SnapAndAlignPartTo(SnapPoint targetSnapPoint)
+    {
+        SnapPoint entrancePoint = GetEntrancePoint();
+        AlignTo(entrancePoint, targetSnapPoint);
+        SnapTo(entrancePoint, targetSnapPoint);
+    }
+
+    private void SnapTo(SnapPoint ownSnapPoint, SnapPoint targetSnapPoint)
+    {
+        var offset = transform.position - ownSnapPoint.transform.position;
+        var newPosition = targetSnapPoint.transform.position + offset;
+        transform.position = newPosition;
+    }
+
+    private void AlignTo(SnapPoint ownSnapPoint, SnapPoint targetSnapPoint)
+    {
+        // Calculate the rotation offset between the level part's current rotation and its own snap point's rotation
+        var rotationOffset = ownSnapPoint.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+
+        // Align the level part's rotation to the target snap point's rotation
+        transform.rotation = targetSnapPoint.transform.rotation;
+
+        // Rotate the level part by 180 degrees to face the correct direction
+        transform.Rotate(0, 180, 0);
+
+        // Apply the rotation offset to fine-tune the alignment
+        transform.Rotate(0, -rotationOffset, 0);
+    }
+
+    public SnapPoint GetEntrancePoint()
+    {
+        return GetSnapPointOfType(SnapPointType.Enter);
+    }
+
+    public SnapPoint GetExitPoint()
+    {
+        return GetSnapPointOfType(SnapPointType.Exit);
+    }
+
+    private SnapPoint GetSnapPointOfType(SnapPointType pointType)
+    {
+        SnapPoint[] snapPoints = GetComponentsInChildren<SnapPoint>();
+        List<SnapPoint> filteredSnapPoints = new List<SnapPoint>();
+
+        // Collect all snap points of the specified type
+        foreach (SnapPoint snapPoint in snapPoints)
+        {
+            if (snapPoint.pointType == pointType)
+            {
+                filteredSnapPoints.Add(snapPoint);
+            }
+        }
+
+        // If there are matching snap points, choose one at random
+        if (filteredSnapPoints.Count > 0)
+        {
+            int randomIndex = Random.Range(0, filteredSnapPoints.Count);
+            return filteredSnapPoints[randomIndex];
+        }
+
+        // If no matching snap points are found, return null
+        return null;
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/SnapPoint.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public enum SnapPointType
+{
+    Enter,
+    Exit,
+}
+public class SnapPoint : MonoBehaviour
+{
+    public SnapPointType pointType;
+
+    private void OnValidate()
+    {
+        gameObject.name = "SnapPoint - " + pointType.ToString();
+    }
+}
