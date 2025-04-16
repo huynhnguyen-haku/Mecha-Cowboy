85b356b3b9b242cbc2f2e4b4c6879f09b30673ec
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-04-16 23:13:40 +0700

    Add limitation zone for level parts

--- Assets/Scripts/ZoneLimitation.cs
+++ Assets/Scripts/ZoneLimitation.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ZoneLimitation : MonoBehaviour
@@ -7,12 +8,35 @@ public class ZoneLimitation : MonoBehaviour
 
     private void Start()
     {
+        GetComponent<MeshRenderer>().enabled = false;
         zoneCollider = GetComponent<BoxCollider>();
         lines = GetComponentsInChildren<ParticleSystem>();
+        ActivateWall(false);
     }
 
-    private void ActivateWall()
+    private void ActivateWall(bool activate)
     {
+        foreach(var line in lines)
+        {
+            if (activate)
+                line.Play();
 
+            else
+                line.Stop();
+        }
+        zoneCollider.isTrigger = !activate;
+    }
+
+    IEnumerator WallActivation()
+    {
+        ActivateWall(true);
+        yield return new WaitForSeconds(1f);
+        ActivateWall(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(WallActivation());
+        Debug.Log("We don't have any business in that area! Don't go near it!");
     }
 }
