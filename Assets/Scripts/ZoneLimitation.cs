882dda1b6d674bf37e32d452657738aaa32ff6e4
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-04-17 14:22:15 +0700

    Backup before prefabs config

--- Assets/Scripts/ZoneLimitation.cs
+++ Assets/Scripts/ZoneLimitation.cs
@@ -3,40 +3,78 @@ using UnityEngine;
 
 public class ZoneLimitation : MonoBehaviour
 {
-    private ParticleSystem[] lines;
     private BoxCollider zoneCollider;
+    private MeshRenderer meshRenderer;
+    private Coroutine fadeCoroutine;
+    private bool isPlayerInside = false;
 
     private void Start()
     {
-        GetComponent<MeshRenderer>().enabled = false;
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
         zoneCollider = GetComponent<BoxCollider>();
-        lines = GetComponentsInChildren<ParticleSystem>();
         ActivateWall(false);
     }
 
     private void ActivateWall(bool activate)
     {
-        foreach(var line in lines)
-        {
-            if (activate)
-                line.Play();
-
-            else
-                line.Stop();
-        }
         zoneCollider.isTrigger = !activate;
     }
 
-    IEnumerator WallActivation()
+    private IEnumerator WallActivation()
     {
         ActivateWall(true);
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeMeshRenderer(true));
+
         yield return new WaitForSeconds(1f);
-        ActivateWall(false);
+
+        if (!isPlayerInside)
+        {
+            ActivateWall(false);
+            if (fadeCoroutine != null)
+                StopCoroutine(fadeCoroutine);
+            fadeCoroutine = StartCoroutine(FadeMeshRenderer(false));
+        }
+    }
+
+    private IEnumerator FadeMeshRenderer(bool fadeIn)
+    {
+        float duration = 1f;
+        float elapsed = 0f;
+        meshRenderer.enabled = true;
+        Color color = meshRenderer.material.color;
+        float startAlpha = fadeIn ? 0f : color.a;
+        float endAlpha = fadeIn ? 1f : 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Lerp(startAlpha, endAlpha, elapsed / duration);
+            meshRenderer.material.color = color;
+            yield return null;
+        }
+
+        color.a = endAlpha;
+        meshRenderer.material.color = color;
+        if (!fadeIn)
+            meshRenderer.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        isPlayerInside = true;
         StartCoroutine(WallActivation());
         Debug.Log("We don't have any business in that area! Don't go near it!");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isPlayerInside = false;
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeMeshRenderer(false));
+        ActivateWall(false);
     }
 }
