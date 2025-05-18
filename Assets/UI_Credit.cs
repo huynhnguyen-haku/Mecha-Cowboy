475fb0f2ea238ab46413514ffe7bbf173adba12d
Huỳnh Hoàng Khôi Nguyên <huynhnguyen1532@gmail.com>
2025-05-18 16:22:46 +0700

    Update ui credit

--- Assets/UI_Credit.cs
+++ Assets/UI_Credit.cs
@@ -1,12 +1,31 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UI_Credit : MonoBehaviour
 {
     [SerializeField] private RectTransform creditText;
 
     private float scrollSpeed = 100f;
-
     private bool isScrolling = true;
+    private Vector2 startPosition; // Vị trí ban đầu của creditText
+
+    private void Awake()
+    {
+        // Gán vị trí ban đầu khi UI được khởi tạo
+        startPosition = creditText.anchoredPosition;
+    }
+
+    private void OnEnable()
+    {
+        // Reset vị trí về ban đầu khi UI được kích hoạt
+        creditText.anchoredPosition = startPosition;
+        isScrolling = true; // Bắt đầu cuộn lại
+    }
+
+    private void OnDisable()
+    {
+        // Tạm dừng cuộn khi UI bị disable
+        isScrolling = false;
+    }
 
     private void Update()
     {
